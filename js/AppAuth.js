3c6e2b0aec767bc5957605221d1d6fb90596ad78
anviiiijain <anvijain250102@gmail.com>
2020-07-08 17:02:04 +0530

    auth not working

--- js/AppAuth.js
+++ js/AppAuth.js
@@ -1,30 +1,3 @@
-(function() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyASvw80DuelGaKx3v3NoK4eo13tT_C1Q7U",
-        authDomain: "dpprojekt-3d1fd.firebaseapp.com",
-        databaseURL: "https://dpprojekt-3d1fd.firebaseio.com",
-        projectId: "dpprojekt-3d1fd",
-        storageBucket: "dpprojekt-3d1fd.appspot.com",
-        messagingSenderId: "455950445352",
-        appId: "1:455950445352:web:c551266cc5d56e5ecd7e1d",
-        measurementId: "G-91EEZ5MBPF"
-    };
-
-    firebase.initializeApp(firebaseConfig);
-
-    //sign in using google
-    function googleSignIn() {
-        provider = new Firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopUp(provider).then(function(result) {
-            console.log(result);
-            console.log("success");
-        }).catch(function(err) {
-            console.log(err);
-            console.log("failed");
-        });
-    }
-
-});
 // const txtEmail = document.getElementById("userEmail");
 // const txtPass = document.getElementById("userPass");
 // const LoginIn = document.getElementById("logInBtn");
